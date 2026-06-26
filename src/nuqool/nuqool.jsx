0767fc4fea31b96c1e4f703578bbee9c7c5c7ad4
Me? <workwithsyedjalal@gmail.com>
2026-06-27 01:18:07 +0530

    update: added nuqool 1-10 feat: added fonts locally

--- src/nuqool/nuqool.jsx
+++ src/nuqool/nuqool.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 
-export const KHUDA_T = "Khuda e Ta'ala";
-export const R_SAWS = "Rasool Allah S.A.W.S";
-export const MAS = "Mahdi e Ma'ud A.S";
-export const HBM = "Hazrat Bandagi Miyan";
-export const BM = "Bandagi Miyan";
+export const KHUDA_T =   "Khuda e Ta'ala";
+export const R_SAWS  =   "Rasool Allah S.A.W.S";
+export const Q       =   "Qur'an"
+export const MAS     =   "Mahdi e Ma'ud A.S";
+export const HMAS    =   "Hazrat Mahdi A.S"
+export const HBM     =   "Hazrat Bandagi Miyan";
+export const BM      =   "Bandagi Miyan";
+
+// Books
+export const MUW     =   "Ma'arij-ul-Wilayat"
+export const SUW     =   "Shawahid-ul-Wilayat"
 
 export const nuqoolObject = {
   1: (
     <p>
-      Naql hai ke Hz. Mahdi-e-Ma'ud A.S ne farmaya agar koi shakhs bande se
-      naql kare toh chahiye ke us naql ko dekhein. Agar Khuda-e-Ta'ala ke
-      kalaam ke muwafiq hai to woh naql bande ki hai, aur agar Khuda-e-Ta'ala
-      ke kalaam ke muwafiq nahi hai to woh naql bande ki nahi, ya woh shakhs
-      hamari baat samjha nahi hai. 
-      <cite> 
-        <br />
-        <br />
-        <br />
+      Naql hai ke Hz. {MAS} ne farmaya agar koi shakhs bande se
+      naql kare toh chahiye ke us naql ko dekhein. Agar {KHUDA_T} ke
+      kalaam ke muwafiq hai to woh naql bande ki hai, aur agar {KHUDA_T} ke kalaam ke muwafiq nahi hai to woh naql bande ki nahi, ya woh shakhs
+      hamari baat samjha nahi hai.
+      <br />
+      <br />
+      <br />
+      <cite>
         Insaaf Nama pgno: 5,
         <br />
         Siraj-ul-Absar pgno. 5,
@@ -28,14 +33,105 @@ export const nuqoolObject = {
   ),
   2:  (
         <p>
-          سْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ ١
+          {HMAS} ne farmaya ke "<strong>hum kisi mazhab ke muqayyad nahi hain aur agar koi hamare sidq ko maloom karna chahta hai to us ko chahiye ke kalaam-e-khuda ki muwafiqat aur {R_SAWS} ki ittiba ka hamare ahwaal wa a'maal me dhoondein aur samajh lein.</strong>"
+          <br />
+          <br />
+          <br />
+          <cite>
+            Aqeeda Sharifa pgno. 4
+          </cite>
         </p>
       ),
   3: (
     <p>
-      Hello
+      {HMAS} ne farmaya ke "<strong>jo shakhs mujhe naql karta hai agar woh naql {KHUDA_T} ke kalaam ke muwafiq hai to woh naql durust hai aur agar woh naql {KHUDA_T} ke kalaam ke muwafiq nahi hai to mujh se nahi hai ya naaqil ka dil sun'ne ke waqt haazir nahi raha hoga. Is liye sahu hua hai.</strong>
+      <br />
+      <br />
+      <br />
+      <cite>
+        Naqliyat {BM} Abdul Rasheed (RZ) pgno. 1
+      </cite>
     </p>
-  )
+  ),
+  4: (
+    <p>
+      {HMAS} ne farmaya ke "<strong>meri naql woh hai jo mutabiq e {Q} ho."</strong>
+      <br/><br/><br/>
+      <cite>
+        {MUW} pgno. 368
+      </cite>
+    </p>
+  ),
+  5: (
+    <p>
+      Naql hai {HBM} Dilawar R.Z ne farmaya <strong>"agar koi shakhs {Q} ki ayat {HMAS} ki naql se tatbeeq dein to (us ka bayan) sahih hai"</strong>
+      <br/>
+      Pas is ibaarat ko {HMAS} ke saamne Sahaba R.Z ne arz kiya. {HMAS} ne farmaya Jo shakhs fana ke darje ko pahuncha ho woh shakhs {Q} ki ayat ko naql se tatbeeq dega.
+      <br/><br/><br/>
+      <cite>Panj Fazail pgno. 100</cite>
+    </p>
+  ),
+  6: (
+    <p>
+      Is majzoob ne apni naak se rassi nikal di us ke ba'ad {HMAS} se sawal kiya ke "{MAS} se koi baat posheeda nahi hai. Allah ki zaat ka bayan karo." {MAS} ne farmaya <strong>"Allah ki zaat bayan mein nahi aati. Allah ke sifaat bayan mein nahi aate hain lekin main beenai ki lazzat bayan karta hoon - 
+      <br /><br />
+      ke kisi shakhs ke naak mein rassi daal kar tamaam zameen par phirayen aur us ko {KHUDA_T} ki beenai suyi ke soraakh mein hojaye to us ko aisi raahat ho phir woh kahe ke hum ko hazaar saal zameen par phirayen taake hum ko aisi raahat ho.</strong>
+      <br /><br /><br />
+      <cite>Hashiya pgno. 199</cite>
+    </p>
+  ),
+  7: (
+    <p>
+      Pas Zunnoon isi tarteeb se aaya jab us ke aane ka balwa yaaron ke kaan mein pahuncha to ba'az yaaron ne kaha ke kis qadr qahhari aawazein aati hain yakayak balwa ka wuqoo hua ke Hindiyon ko qatl karne ke liye Meer Zunnoon aa raha hai us waqt ek mard (mard-e-khuda) ne {HMAS} ke huzoor mein arz kiya ke Aye Khudawand badshah ka wazir aata hai. Imam {MAS} ne us par tang (bezar) aakar farmaya ke badshah ek hai us ko wazir ki haajat nahi.
+      <br /><br /><br />
+      <cite>
+        Hujjat-ul-Munsafeen pgno. 28
+        <br />
+        Jannat-ul-Wilayat pgno. 48
+        <br />
+        {SUW} pgno. 224
+        <br />
+        {MUW} pgno. 164
+      </cite>
+    </p>
+  ),
+  8: (
+    <p>
+      Naql hai {HMAS} ne farmaya ma'roof Allah hi ki zaat hai.
+      <br /><br /><br />
+      <cite>
+        Naqliyat Miyan Syed Alam (RH) pgno. 12
+      </cite>
+    </p>
+  ),
+  9: (
+    <p>
+      Naql hai {HMAS} ne farmaya ke Aye baar-e-Khuda tu qaadir hai ke Adam A.S ko tu ne mitti se paida kiya aur Nooh A.S, Ibrahim A.S, Moosa A.S, Isa A.S aur Muhammad S.A.W.S ko bhi tune mitti se paida kiya aur {MAS} ko bhi tune mitti se paida kiya.
+      <br /><br /><br />
+      <cite>
+        Naqliyat Miyan Syed Alam (RH) pgno. 87
+        <br />
+        Matla-ul-Wilayat pgno. 59
+        <br />
+        {MUW} pgno. 74
+        </cite>
+    </p>
+  ),
+  10: (
+    <p>
+      Naql hai ke {HMAS} ne Qazi Qadan se poochha ke tum kahan ke qazi ho unhon ne jawab diya ke mulk-e-Sindh ka qazi hoon phir 
+      <br /><br />{HMAS} ne farmaya ke Sindh kis ka mulk hai unhon ne jawab diya ke Jaam ka. 
+      <br /><br />Phir AanHazrat A.S ne farmaya ke Jaam kis ka hai. Unhon ne jawab diya ke {KHUDA_T} ka hai. 
+      <br /><br />Phir AanHazrat A.S ne farmaya ke {KHUDA_T} kis ka hai unhon ne jawab diya ke yahan tak apne ilm se main ne jawabaat diye is jagah hamara ilm khatam ho chuka hai jo kuch Khundkar farmayein wahi tehqeeq hai. 
+      <br /><br />Aan Hazrat A.S ne farmaya <strong>"Aye Qazi {KHUDA_T} usi ka hai jo {KHUDA_T} ko haasil kare."</strong>
+      <br /><br /><br />
+      <cite>
+        {SUW} pgno. 199
+        <br />
+        {MUW} pgno. 148
+      </cite>
+    </p>
+  ),
   // Add more nuqool entries here as: 2: (<p>...</p>)
 };
 
